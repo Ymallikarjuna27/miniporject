4d3671d58bf11790c2267d857add2dbaea8ac9c7
Mallikarjuna <yampalaku.mallikarjuna@capgemini.com>
2026-05-04 16:26:57 +0530

    Updated Streamlit app with upload and download

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import pandas as pd
 
 st.title("Mini Data Processing Tool")
 
-# File Upload
+# Upload input file
 uploaded_file = st.file_uploader(
     "Upload input CSV file",
     type=["csv"]
 )
 
 if uploaded_file is not None:
-    # Read uploaded file
     df = pd.read_csv(uploaded_file)
 
     st.subheader("Uploaded Data")
@@ -20,7 +19,6 @@ if uploaded_file is not None:
     df["result"] = df["marks"].apply(
         lambda x: "Pass" if x >= 70 else "Fail"
     )
-
     average_marks = round(df["marks"].mean(), 2)
 
     st.subheader("Processed Data")
@@ -29,9 +27,8 @@ if uploaded_file is not None:
     st.subheader("Summary")
     st.write("Average Marks:", average_marks)
 
-    # Download processed file
+    # Download file
     csv_data = df.to_csv(index=False).encode("utf-8")
-
     st.download_button(
         label="Download Processed CSV",
         data=csv_data,
